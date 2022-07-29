e98da2742424ab40803dca422fb0d5aa99556d08
Tushar Upadhyay <tushar.u110@gmail.com>
2022-07-29 13:58:08 +0000

    lvl2 v2

---
+++ level2_design/README.md
@@ -0,0 +1,165 @@
+# Level 2 - Tushar Upadhyay
+
+![](https://imgur.com/ZhBRtaD.png)
+
+## Verification Environment
+
+Verifying a big processor gives out many errors; hence, we require a separate file for logging all errors. This block of code is responsible for creating the file and removing it from a previous run if it exists. The file is created in the same directory as the python file.
+```
+if os.path.exists("errors.txt"):
+  os.remove("errors.txt")
+else:
+  print("The file does not exist") 
+f = open("errors.txt","x")
+f.close()
+```
+These functions are defined making it easier for the user to create R-type, R4-type and I-type instructions.
+```
+def Rtype(funct7, funct3, opcode)
+def Itype(funct7_imm, imm, funct3, opcode)
+def I_func_type(funct7, imm_value1, func3, opcode)
+def R4type(funct2,funct3,opcode)
+```
+Constructs a list with errors in a tabulated form.
+```
+def errorLog(dut, dut_output, expected_mav_putvalue, instr)
+```
+Writes all the errors to the text file.
+```
+def errorWrite(funcs_logged, errorList):
+```
+Different tests are defined for different types of instruction where different formats of inputs to the DUT are required. Namely,
+```
+test_0110011_Rtype 
+test_0010011_Itype             
+test_0010011_Ifunc              
+test_0110011_R4type
+test_0010011_FSRI_SHFLI_UNSHFLI
+```
+All of these tests have slightly different codes, but by the majority, all of these tests are coded similarly.
+Hence we will only be evaluating `test_0110011_Rtype` in this document (other functions can be understood by reading the comments in the source code itself).
+`opcode` stores the opcode for the group of instructions
+```
+opcode = 0b0110011
+cocotb.fork(clock_gen(dut.CLK))
+
+# reset
+dut.RST_N.value <= 0
+yield Timer(10) 
+dut.RST_N.value <= 1
+```
+
+`test_vals` contains all the values inputted to the DUT as operands.
+`operations` is a list that contains all the instructions that need to be executed.
+`errors` list stores all the errors when the testbench runs.
+`testInstructions` String that is appended to the `errors.txt` file to indicate the Instructions on which the tests were performed.
+```
+    test_vals = [0x0,0x1,0x55555555,0xAAAAAAAA,0x7fffffff,0xfffffffe,0xfffffff]
+
+    operations = list()
+
+    operations.append(Rtype(0b0100000, 0b111, opcode))    # ANDN
+    operations.append(Rtype(0b0100000, 0b110, opcode))    # ORN
+    operations.append(Rtype(0b0100000, 0b100, opcode))    # XNOR
+    ...
+    ...
+    testInstructions = "ANDN ORN XNOR ... "
+    errors = list()
+```
+The **for** loops iterate all the instructions stored in `operations` and all the values stored in `test_vals`, and input it to the DUT. Src1 and Src2 both iterate over `test_vals` while Src3 which is not used by Rtype instructions, is fed with random values to ensure the design works fine.
+After every calculation, the expected and actual values are equated; if not, they get appended to the `errors` list.
+At the end the `errors` list is passed to the `errorWrite` function which writes all the errors to the `errors.txt` file.
+```
+for op in operations:
+    for src1_val in test_vals:
+        for src2_val in test_vals:
+            mav_putvalue_src1 = src1_val
+            mav_putvalue_src2 = src2_val
+            mav_putvalue_src3 = random.randint(0, (1<<32)-1)
+            mav_putvalue_instr = op
+
+            expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+            
+            dut.mav_putvalue_src1.value = mav_putvalue_src1
+            dut.mav_putvalue_src2.value = mav_putvalue_src2
+            dut.mav_putvalue_src3.value = mav_putvalue_src3
+            dut.EN_mav_putvalue.value = 1
+            dut.mav_putvalue_instr.value = mav_putvalue_instr
+            yield Timer(1)
+
+            dut_output = dut.mav_putvalue.value
+
+            if dut_output != expected_mav_putvalue:
+                errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr))
+assert errorWrite(testInstructions, errors)
+```
+
+**Format of `errors.txt`**
+The first line lists all the instructions tested, and if errors are found, they are listed in the table below. Else a "NO Errors Detected" is written.
+Example - 
+```
+CMIX CMOV FSL FSR
+
+DUT OUTPUT    EXPECTED OUTPUT    SRC1    SRC2    SRC3    INST
+------------  -----------------  ------  ------  ------  ------
+NO Errors Detected
+```
+
+## Test Scenario
+Errors logged in the `errors.txt`
+```
+DUT OUTPUT    EXPECTED OUTPUT    SRC1        SRC2        SRC3                                    INST
+------------  -----------------  ----------  ----------  ----------  --------------------------------
+0x1           0x3                0x1         0x0         0xa4094c5d  01000000000000000111000000110011
+0x3           0x1                0x1         0x1         0xaf8b282e  01000000000000000111000000110011
+0x3           0x1                0x1         0x55555555  0xc5ce72d1  01000000000000000111000000110011
+0x1           0x3                0x1         0xaaaaaaaa  0x96643cb1  01000000000000000111000000110011
+0x3           0x1                0x1         0x7fffffff  0xbd0d1f0a  01000000000000000111000000110011
+0x1           0x3                0x1         0xfffffffe  0x4df9b24d  01000000000000000111000000110011
+0x3           0x1                0x1         0xfffffff   0x52a4db3b  01000000000000000111000000110011
+0x1           0xaaaaaaab         0x55555555  0x0         0xbd54ba8f  01000000000000000111000000110011
+0x3           0xaaaaaaa9         0x55555555  0x1         0xfbc19e36  01000000000000000111000000110011
+0xaaaaaaab    0x1                0x55555555  0x55555555  0x1f204ee3  01000000000000000111000000110011
+0x1           0xaaaaaaab         0x55555555  0xaaaaaaaa  0x4427cf24  01000000000000000111000000110011
+0xaaaaaaab    0x1                0x55555555  0x7fffffff  0xb35e1e78  01000000000000000111000000110011
+0xaaaaaaa9    0x3                0x55555555  0xfffffffe  0xd8b185ff  01000000000000000111000000110011
+0xaaaaaab     0xa0000001         0x55555555  0xfffffff   0x3df120bc  01000000000000000111000000110011
+0x1           0x155555555        0xaaaaaaaa  0x0         0x2c051ddb  01000000000000000111000000110011
+0x1           0x155555555        0xaaaaaaaa  0x1         0x454484c1  01000000000000000111000000110011
+0x1           0x155555555        0xaaaaaaaa  0x55555555  0xb7689680  01000000000000000111000000110011
+0x155555555   0x1                0xaaaaaaaa  0xaaaaaaaa  0x2e840171  01000000000000000111000000110011
+0x55555555    0x100000001        0xaaaaaaaa  0x7fffffff  0x15871d88  01000000000000000111000000110011
+0x155555555   0x1                0xaaaaaaaa  0xfffffffe  0x11dcfa34  01000000000000000111000000110011
+0x15555555    0x140000001        0xaaaaaaaa  0xfffffff   0x8d738a3f  01000000000000000111000000110011
+0x1           0xffffffff         0x7fffffff  0x0         0xfde26c34  01000000000000000111000000110011
+0x3           0xfffffffd         0x7fffffff  0x1         0x5c9f96cf  01000000000000000111000000110011
+0xaaaaaaab    0x55555555         0x7fffffff  0x55555555  0x2ec9e068  01000000000000000111000000110011
+0x55555555    0xaaaaaaab         0x7fffffff  0xaaaaaaaa  0x6ad4fd3   01000000000000000111000000110011
+0xffffffff    0x1                0x7fffffff  0x7fffffff  0x31aaa926  01000000000000000111000000110011
+0xfffffffd    0x3                0x7fffffff  0xfffffffe  0x58c01b60  01000000000000000111000000110011
+0x1fffffff    0xe0000001         0x7fffffff  0xfffffff   0x476c1557  01000000000000000111000000110011
+0x1           0x1fffffffd        0xfffffffe  0x0         0x6c2e1809  01000000000000000111000000110011
+0x1           0x1fffffffd        0xfffffffe  0x1         0xd5bccc1f  01000000000000000111000000110011
+0xaaaaaaa9    0x155555555        0xfffffffe  0x55555555  0x7f3b713   01000000000000000111000000110011
+0x155555555   0xaaaaaaa9         0xfffffffe  0xaaaaaaaa  0x2b64a58e  01000000000000000111000000110011
+0xfffffffd    0x100000001        0xfffffffe  0x7fffffff  0xb2744227  01000000000000000111000000110011
+0x1fffffffd   0x1                0xfffffffe  0xfffffffe  0xd156573a  01000000000000000111000000110011
+0x1ffffffd    0x1e0000001        0xfffffffe  0xfffffff   0xc5386ae3  01000000000000000111000000110011
+0x1           0x1fffffff         0xfffffff   0x0         0xc7926252  01000000000000000111000000110011
+0x3           0x1ffffffd         0xfffffff   0x1         0x38ac6aa6  01000000000000000111000000110011
+0xaaaaaab     0x15555555         0xfffffff   0x55555555  0x8f738978  01000000000000000111000000110011
+0x15555555    0xaaaaaab          0xfffffff   0xaaaaaaaa  0x4051e119  01000000000000000111000000110011
+0x1fffffff    0x1                0xfffffff   0x7fffffff  0x94dd2733  01000000000000000111000000110011
+0x1ffffffd    0x3                0xfffffff   0xfffffffe  0x59b5b1c6  01000000000000000111000000110011
+0x1fffffff    0x1                0xfffffff   0xfffffff   0xee6bd625  01000000000000000111000000110011
+```
+
+All of these are errors are logged for the **`ANDN`** instruction
+
+![](https://imgur.com/bRAGCqF.png)
+
+## Verification Strategy
+To reduce the testing time, the inputs generated to the DUT are all edge cases and are not random inputs. Otherwise, the real-time execution would take a tremendous amount of time. The same inputs are given to the DUT and the model python file and are equated at the end.
+
+## Is the verification complete ?
+While testing, an assumption is made that rs1, rs2, rs3, and rd fields in an instruction do not cause any change to the output. All the operands are only taken from the `mav_putvalue_src1`, `mav_putvalue_src2` and `mav_putvalue_src3`. The inputs are tested over a small range of 49 to 343 different values of each instruction (depending on the test function). With the small range, the inputs are such that, every bit of the input assumes a value of 0 or 1 over the tests. Therefore, the verification is complete with the assumption made.
--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -29,6 +29,9 @@ def Rtype(funct7, funct3, opcode):
 def Itype(funct7_imm, imm, funct3, opcode):
     return ((funct7_imm << 27)|(imm << 20)|(funct3 << 12)|opcode)
 
+def I_func_type(funct7, imm_value1, func3, opcode):
+    return ((funct7 << 25)|(imm_value1 << 20)|(func3 << 12)|opcode)
+
 def R4type(funct2,funct3,opcode):
     return ((funct2 << 25)|(funct3 << 12)|(opcode))
 
@@ -67,7 +70,7 @@ def clock_gen(signal):
         yield Timer(1) 
 
 @cocotb.test()
-def test_0110011_simple_operations(dut):
+def test_0110011_Rtype(dut):
     opcode = 0b0110011
     cocotb.fork(clock_gen(dut.CLK))
 
@@ -83,96 +86,36 @@ def test_0110011_simple_operations(dut):
     operations.append(Rtype(0b0100000, 0b111, opcode))    # ANDN
     operations.append(Rtype(0b0100000, 0b110, opcode))    # ORN
     operations.append(Rtype(0b0100000, 0b100, opcode))    # XNOR
-
-    typeOfOperation = {'111':'ANDN', '110':'ORN', '100':'XNOR'}
-
-    errors = list()
-
-    for op in operations:
-        for src1_val in test_vals:
-            for src2_val in test_vals:
-                mav_putvalue_src1 = src1_val
-                mav_putvalue_src2 = src2_val
-                mav_putvalue_instr = op
-                expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, 0)
-                
-                dut.mav_putvalue_src1.value = mav_putvalue_src1
-                dut.mav_putvalue_src2.value = mav_putvalue_src2
-                dut.mav_putvalue_src3.value = 0
-                dut.EN_mav_putvalue.value = 1
-                dut.mav_putvalue_instr.value = mav_putvalue_instr
-                yield Timer(1)
-
-                dut_output = dut.mav_putvalue.value
-
-                if dut_output != expected_mav_putvalue:
-                    errors.append(errorLog(dut, dut_output, expected_mav_putvalue,typeOfOperation[dut.mav_putvalue_instr.value.binstr[-15:-12]]))
-
-    assert errorWrite("ANDN ORN XNOR", errors)
-
-
-
-@cocotb.test()
-def test_0110011_shift_operations(dut):
-    opcode = 0b0110011
-    cocotb.fork(clock_gen(dut.CLK))
-
-    # reset
-    dut.RST_N.value <= 0
-    yield Timer(10) 
-    dut.RST_N.value <= 1
- 
-    test_vals = [0x0,0x1,0x55555555,0xAAAAAAAA,0x7fffffff,0xfffffffe,0xfffffff]
-    operations = list()
-
     operations.append(Rtype(0b0010000, 0b001, opcode))    # SLO
     operations.append(Rtype(0b0010000, 0b101, opcode))    # SRO
     operations.append(Rtype(0b0110000, 0b001, opcode))    # ROL
     operations.append(Rtype(0b0110000, 0b101, opcode))    # ROR
-
-    errors = list()
-
-    for op in operations:
-        for src1_val in test_vals:
-            for src2_val in test_vals:
-                mav_putvalue_src1 = src1_val
-                mav_putvalue_src2 = src2_val
-                mav_putvalue_instr = op
-                expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, 0)
-                
-                dut.mav_putvalue_src1.value = mav_putvalue_src1
-                dut.mav_putvalue_src2.value = mav_putvalue_src2
-                dut.mav_putvalue_src3.value = 0
-                dut.EN_mav_putvalue.value = 1
-                dut.mav_putvalue_instr.value = mav_putvalue_instr
-                yield Timer(1)
-
-                dut_output = dut.mav_putvalue.value
-
-                if dut_output != expected_mav_putvalue:
-                    errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr[-15:-12]))
-    assert errorWrite("SLO SRO ROL", errors)
-
-
-@cocotb.test()
-def test_0110011_SB_operations(dut):
-    opcode = 0b0110011
-    cocotb.fork(clock_gen(dut.CLK))
-
-    # reset
-    dut.RST_N.value <= 0
-    yield Timer(10) 
-    dut.RST_N.value <= 1
-
-    test_vals = [0x0,0x1,0x55555555,0xAAAAAAAA,0x7fffffff,0xfffffffe,0xfffffff]
-    operations = list()
-
     operations.append(Rtype(0b0100100, 0b001, opcode))    # SBCLR
-    operations.append(Rtype(0b0010100, 0b101, opcode))    # SBSET
+    operations.append(Rtype(0b0010100, 0b001, opcode))    # SBSET
     operations.append(Rtype(0b0110100, 0b001, opcode))    # SBINV
     operations.append(Rtype(0b0100100, 0b101, opcode))    # SBEXT
     operations.append(Rtype(0b0010100, 0b101, opcode))    # GORC
     operations.append(Rtype(0b0110100, 0b101, opcode))    # GREV
+    operations.append(Rtype(0b0010000, 0b010, opcode))    # SH1ADD
+    operations.append(Rtype(0b0010000, 0b100, opcode))    # SH2ADD
+    operations.append(Rtype(0b0010000, 0b110, opcode))    # SH3ADD
+    operations.append(Rtype(0b0000101, 0b001, opcode))    # CLMUL
+    operations.append(Rtype(0b0000101, 0b011, opcode))    # CLMULH
+    operations.append(Rtype(0b0000101, 0b010, opcode))    # CLMULR
+    operations.append(Rtype(0b0000101, 0b100, opcode))    # MIN
+    operations.append(Rtype(0b0000101, 0b101, opcode))    # MAX
+    operations.append(Rtype(0b0000101, 0b110, opcode))    # MINU
+    operations.append(Rtype(0b0000101, 0b111, opcode))    # MAXU
+    operations.append(Rtype(0b0100100, 0b110, opcode))    # BDEP
+    operations.append(Rtype(0b0000100, 0b110, opcode))    # BEXT
+    operations.append(Rtype(0b0000100, 0b100, opcode))    # PACK
+    operations.append(Rtype(0b0100100, 0b100, opcode))    # PACKU
+    operations.append(Rtype(0b0000100, 0b111, opcode))    # PACKH
+    operations.append(Rtype(0b0000100, 0b001, opcode))    # SHFL
+    operations.append(Rtype(0b0000100, 0b101, opcode))    # UNSHFL
+    operations.append(Rtype(0b0100100, 0b111, opcode))    # BFP
+
+    testInstructions = "ANDN ORN XNOR SLO SRO ROL ROR SBCLR SBSET SBINV SBEXT GORC GREV SH1ADD SH2ADD SH3ADD CLMUL CLMULH CLMULR MIN MAX MINU MAXU BDEP BEXT PACK PACKU PACKH SHFL UNSHFL BFP"
 
     errors = list()
 
@@ -181,12 +124,14 @@ def test_0110011_SB_operations(dut):
             for src2_val in test_vals:
                 mav_putvalue_src1 = src1_val
                 mav_putvalue_src2 = src2_val
+                mav_putvalue_src3 = random.randint(0, (1<<32)-1)
                 mav_putvalue_instr = op
-                expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, 0)
+
+                expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
                 
                 dut.mav_putvalue_src1.value = mav_putvalue_src1
                 dut.mav_putvalue_src2.value = mav_putvalue_src2
-                dut.mav_putvalue_src3.value = 0
+                dut.mav_putvalue_src3.value = mav_putvalue_src3
                 dut.EN_mav_putvalue.value = 1
                 dut.mav_putvalue_instr.value = mav_putvalue_instr
                 yield Timer(1)
@@ -194,13 +139,11 @@ def test_0110011_SB_operations(dut):
                 dut_output = dut.mav_putvalue.value
 
                 if dut_output != expected_mav_putvalue:
-                    errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr[-15:-12]))
-    assert errorWrite("SBCLR SBSET SBINV SBEXT GORC GREV", errors)
-
-
+                    errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr))
+    assert errorWrite(testInstructions, errors)
 
 @cocotb.test()
-def test_0010011_shift_operations(dut):
+def test_0010011_Itype(dut):
     opcode = 0b0010011
     cocotb.fork(clock_gen(dut.CLK))
 
@@ -210,14 +153,23 @@ def test_0010011_shift_operations(dut):
     dut.RST_N.value <= 1
 
     test_vals = [0x0,0x1,0x55555555,0xAAAAAAAA,0x7fffffff,0xfffffffe,0xfffffff]
+
     operations = list()
 
     src1_vals = test_vals
     imm = [0b0000000,0b0000001,0b0101010,0b1010101,0b0111111,0b1111110,0b1111111]
 
-    operations.append(Itype(0b00100,0b0, 0b001, opcode))    # SLOI
-    operations.append(Itype(0b00100,0b0, 0b101, opcode))    # SROI
-    operations.append(Itype(0b01100,0b0, 0b101, opcode))    # RORI
+    operations.append(Itype(0b00100, 0b0, 0b001, opcode))    # SLOI
+    operations.append(Itype(0b00100, 0b0, 0b101, opcode))    # SROI
+    operations.append(Itype(0b01100, 0b0, 0b101, opcode))    # RORI
+    operations.append(Itype(0b01001, 0b0, 0b001, opcode))    # SBCLRI
+    operations.append(Itype(0b00101, 0b0, 0b001, opcode))    # SBSETI
+    operations.append(Itype(0b01101, 0b0, 0b001, opcode))    # SBINVI
+    operations.append(Itype(0b01001, 0b0, 0b101, opcode))    # SBEXTI
+    operations.append(Itype(0b00101, 0b0, 0b101, opcode))    # GORCI
+    operations.append(Itype(0b01101, 0b0, 0b101, opcode))    # GREVI
+
+    testInstructions = "SLOI SROI RORI SBCLRI SBSETI SBINVI SBEXTI GORCI GREVI"
 
     errors = list()
 
@@ -225,12 +177,16 @@ def test_0010011_shift_operations(dut):
         for src1_val in test_vals:
             for imm_val in imm:
                 mav_putvalue_src1 = src1_val
+                mav_putvalue_src2 = random.randint(0, (1<<32)-1)
+                mav_putvalue_src3 = random.randint(0, (1<<32)-1)
+
                 mav_putvalue_instr = op|(imm_val<<20)
-                expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, 0, 0)
+                expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
                 
                 dut.mav_putvalue_src1.value = mav_putvalue_src1
-                dut.mav_putvalue_src2.value = 0
-                dut.mav_putvalue_src3.value = 0
+                dut.mav_putvalue_src2.value = mav_putvalue_src2
+                dut.mav_putvalue_src3.value = mav_putvalue_src3
+
                 dut.EN_mav_putvalue.value = 1
                 dut.mav_putvalue_instr.value = mav_putvalue_instr
                 yield Timer(1)
@@ -238,11 +194,11 @@ def test_0010011_shift_operations(dut):
                 dut_output = dut.mav_putvalue.value
 
                 if dut_output != expected_mav_putvalue:
-                    errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr[-15:-12]))
-    assert errorWrite("SLOI SROI RORI", errors)
+                    errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr))
+    assert errorWrite(testInstructions, errors)
 
 @cocotb.test()
-def test_0010011_SB_Imm_operations(dut):
+def test_0010011_Ifunc(dut):
     opcode = 0b0010011
     cocotb.fork(clock_gen(dut.CLK))
 
@@ -252,42 +208,49 @@ def test_0010011_SB_Imm_operations(dut):
     dut.RST_N.value <= 1
 
     test_vals = [0x0,0x1,0x55555555,0xAAAAAAAA,0x7fffffff,0xfffffffe,0xfffffff]
+
     operations = list()
 
-    src1_vals = test_vals
-    imm = [0b0000000,0b0000001,0b0101010,0b1010101,0b0111111,0b1111110,0b1111111]
+    operations.append(I_func_type(0b0110000, 0b00000, 0b001, opcode))    # CLZ
+    operations.append(I_func_type(0b0110000, 0b00001, 0b001, opcode))    # CTZ
+    operations.append(I_func_type(0b0110000, 0b00010, 0b001, opcode))    # PCNT
+    operations.append(I_func_type(0b0110000, 0b00100, 0b001, opcode))    # SEXT.B
+    operations.append(I_func_type(0b0110000, 0b00101, 0b001, opcode))    # SEXT.H
+    operations.append(I_func_type(0b0110000, 0b10000, 0b001, opcode))    # CRC32.B
+    operations.append(I_func_type(0b0110000, 0b10001, 0b001, opcode))    # CRC32.H
+    operations.append(I_func_type(0b0110000, 0b10010, 0b001, opcode))    # CRC32.W
+    operations.append(I_func_type(0b0110000, 0b11000, 0b001, opcode))    # CRC32C.B
+    operations.append(I_func_type(0b0110000, 0b11001, 0b001, opcode))    # CRC32C.H
+    operations.append(I_func_type(0b0110000, 0b11010, 0b001, opcode))    # CRC32C.W
 
-    operations.append(Itype(0b01001, 0, 0b001, opcode))    # SBCLRI
-    operations.append(Itype(0b00101, 0, 0b001, opcode))    # SBSETI
-    operations.append(Itype(0b01101, 0, 0b001, opcode))    # SBINVI
-    operations.append(Itype(0b01001, 0, 0b101, opcode))    # SBEXTI
-    operations.append(Itype(0b00101, 0, 0b101, opcode))    # GORCI
-    operations.append(Itype(0b01101, 0, 0b101, opcode))    # GREVI
+    testInstructions = "CLZ CTZ PCNT SEXT.B SEXT.H CRC32.B CRC32.H CRC32.W CRC32C.B CRC32C.H CRC32C.W"
 
     errors = list()
 
     for op in operations:
         for src1_val in test_vals:
-            for imm_val in imm:
-                mav_putvalue_src1 = src1_val
-                mav_putvalue_instr = op|(imm_val<<20)
-                expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, 0, 0)
-                
-                dut.mav_putvalue_src1.value = mav_putvalue_src1
-                dut.mav_putvalue_src2.value = 0
-                dut.mav_putvalue_src3.value = 0
-                dut.EN_mav_putvalue.value = 1
-                dut.mav_putvalue_instr.value = mav_putvalue_instr
-                yield Timer(1)
+            mav_putvalue_src1 = src1_val
+            mav_putvalue_src2 = random.randint(0, (1<<32)-1)
+            mav_putvalue_src3 = random.randint(0, (1<<32)-1)
 
-                dut_output = dut.mav_putvalue.value
+            mav_putvalue_instr = op
+            expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+            
+            dut.mav_putvalue_src1.value = mav_putvalue_src1
+            dut.mav_putvalue_src2.value = mav_putvalue_src2
+            dut.mav_putvalue_src3.value = mav_putvalue_src3
+            dut.EN_mav_putvalue.value = 1
+            dut.mav_putvalue_instr.value = mav_putvalue_instr
+            yield Timer(1)
 
-                if dut_output != expected_mav_putvalue:
-                    errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr[-15:-12]))
-    assert errorWrite("SBCLRI SBSETI SBINVI SBEXTI GORCI GREVI", errors)
+            dut_output = dut.mav_putvalue.value
+
+            if dut_output != expected_mav_putvalue:
+                errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr))
+    assert errorWrite(testInstructions, errors)
 
 @cocotb.test()
-def test_0110011__Rtype_operations(dut):
+def test_0110011_R4type(dut):
     opcode = 0b0110011
     cocotb.fork(clock_gen(dut.CLK))
 
@@ -303,6 +266,8 @@ def test_0110011__Rtype_operations(dut):
     operations.append(R4type(0b11, 0b101, opcode))    # CMOV
     operations.append(R4type(0b10, 0b001, opcode))    # FSL
     operations.append(R4type(0b10, 0b101, opcode))    # FSR
+
+    testInstructions = "CMIX CMOV FSL FSR"
 
     errors = list()
 
@@ -326,11 +291,11 @@ def test_0110011__Rtype_operations(dut):
                     dut_output = dut.mav_putvalue.value
 
                     if dut_output != expected_mav_putvalue:
-                        errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr[-15:-12]))
-    assert errorWrite("CMIX CMOV FSL FSR", errors)
+                        errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr))
+    assert errorWrite(testInstructions, errors)
 
 @cocotb.test()
-def test_0010011__FSRI_operations(dut):
+def test_0010011_FSRI_SHFLI_UNSHFLI(dut):
     opcode = 0b0010011
     cocotb.fork(clock_gen(dut.CLK))
 
@@ -341,10 +306,14 @@ def test_0010011__FSRI_operations(dut):
 
     test_vals = [0x0,0x1,0x55555555,0xAAAAAAAA,0x7fffffff,0xfffffffe,0xfffffff]
 
+    imm = [0b000000,0b000001,0b010101,0b101010,0b011111,0b111110,0b111111]
+
     operations = list()
     operations.append((0b1 << 26)|(0b101 << 12)|opcode)        # FSRI
+    operations.append((0b000010 << 26)|(0b001 << 12)|opcode)   # SHFLI
+    operations.append((0b000010 << 26)|(0b101 << 12)|opcode)   # UNSHFLI
     
-    imm = [0b000000,0b000001,0b010101,0b101010,0b011111,0b111110,0b111111]
+    testInstructions = "FSRI SHFLI UNSHFLI"
 
     errors = list()
 
@@ -353,7 +322,7 @@ def test_0010011__FSRI_operations(dut):
             for src3_val in test_vals:
                 for imm_val in imm:
                     mav_putvalue_src1 = src1_val
-                    mav_putvalue_src2 = 0
+                    mav_putvalue_src2 = random.randint(0, (1<<32)-1)
                     mav_putvalue_src3 = src3_val
                     mav_putvalue_instr = op|(imm_val << 20)
                     expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
@@ -368,5 +337,5 @@ def test_0010011__FSRI_operations(dut):
                     dut_output = dut.mav_putvalue.value
 
                     if dut_output != expected_mav_putvalue:
-                        errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr[-15:-12]))
-    assert errorWrite("FSRI", errors)
+                        errors.append(errorLog(dut, dut_output, expected_mav_putvalue,dut.mav_putvalue_instr.value.binstr))
+    assert errorWrite(testInstructions, errors)
